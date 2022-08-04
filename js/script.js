a3ddf4c7b840697157efbc2d870b612fe8adf2c3
serlyNazarian <serlynazarian@gmail.com>
2022-08-04 17:08:45 +0400

    main

---
+++ js/script.js
@@ -0,0 +1,18 @@
+function validateEmail(email) {
+    let res = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return res.test(email);
+  }
+  function validate() {
+    let result = $("#result");
+    let email = $("#email").val();
+    result.text("");
+    if(validateEmail(email)) {
+      result.text(email + " is valid");
+      result.css("color", "blue");
+    } else {
+      result.text(email + " is not valid");
+      result.css("color", "red");
+    }
+    return false;
+  }
+  $("#validate").on("click", validate);
